9d69c66a8730b24813149619024c0f97932f855b
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-11-05 15:00:10 -0700

    Buttons styles done. Added some js for some functionality

--- 01-Neumorphism/app.js
+++ 01-Neumorphism/app.js
@@ -1,7 +1,17 @@
 // Selectors
 const toggleThemeBtn = document.querySelector('.fa-circle-half-stroke');
+const powerBtn = document.querySelector('#powerBtn');
+const powerBtnContent = document.querySelector('.fa-power-off');
+const contentCtn = document.querySelector('.container');
 // Adding eventlistener to button for toggle theme
 toggleThemeBtn.addEventListener('click', () => {
     document.body.classList.toggle('toggleTheme');
     toggleThemeBtn.classList.toggle('toggleThemeBtn');
+    contentCtn.classList.toggle('toggleThemeCtn');
+});
+powerBtn.addEventListener('mouseover', () => {
+   powerBtnContent.classList.add('toggleThemeBtn');
+});
+powerBtn.addEventListener('mouseout', () => {
+   powerBtnContent.classList.remove('toggleThemeBtn');
 });
